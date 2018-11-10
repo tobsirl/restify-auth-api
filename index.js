d17b0ceb6bd6a964cec66d0a7b5f058663ee32c1
Paul Tobin <tobster111@gmail.com>
2018-11-10 11:57:38 +0000

    Setup server added first route

---
+++ index.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+const restify = require('restify');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const server = restify.createServer();
+
+// Middleware
+server.use(restify.plugins.bodyParser());
+
+// Server
+server.listen(port, () => {
+  mongoose.connect(
+    process.env.mongoDB,
+    { useNewUrlParser: true }
+  );
+});
+
+const db = mongoose.connection;
+
+db.on('error', err => console.log(err));
+
+db.once('open', () => {
+  require('./routes/customers')(server);
+  console.log(`Server running on port ${port}`);
+});
