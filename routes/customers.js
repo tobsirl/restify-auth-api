17168b304a06e4b6d298f8e53bdb06a181d39e23
Paul Tobin <tobster111@gmail.com>
2018-11-10 12:17:32 +0000

    Added customers model + get all customers

--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,18 @@
 const errors = require('restify-errors');
+const Customer = require('../models/Customer');
 
 module.exports = server => {
-  server.get('/customers', (req, res, next) => {
-    res.send({ msg: 'Testing route' });
-    next();
+  // @route   GET /customers
+  // @desc    Get all customers
+  // @access  Public
+  server.get('/customers', async (req, res, next) => {
+    // res.send({ msg: 'Testing route' });
+    try {
+      const customer = await Customer.find({});
+      res.send(customer);
+      next();
+    } catch (err) {
+      return next(new errors.InvalidContentError(err));
+    }
   });
 };
